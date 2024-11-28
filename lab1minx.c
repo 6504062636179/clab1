5620533a68b3893c7bca617e569bd77744997f05
6504062636179 <153050109+6504062636179@users.noreply.github.com>
2024-11-28 18:20:22 +0700

    making scanf n1 and n2 the same line

--- lab1minx.c
+++ lab1minx.c
@@ -5,11 +5,7 @@ int main(){
 	
 	int n1 ;
 	int n2 ;
-	printf("n1=");
-	scanf("%d",&n1);
-	printf("n2=");
-	scanf("%d",&n2);
-
+	scanf("%d %d",&n1,&n2);
 	if(n1 > n2){
 		printf("n1 is greater than n2 %d",n1-n2);
 	}else{
