61d10ae04c68580167cf1d5e34fc70b0d67691d1
6504062636179 <153050109+6504062636179@users.noreply.github.com>
2024-11-28 18:22:57 +0700

    making scanf n1 and n2 not the same line

--- lab1minx.c
+++ lab1minx.c
@@ -3,12 +3,12 @@
 
 int main(){
 	
-	int n1 ;
-	int n2 ;
-	scanf("%d %d",&n1,&n2);
+	int n1;
+	int n2;
+	scanf("%d \n %d",&n1,&n2);
 	if(n1 > n2){
-		printf("n1 is greater than n2 %d",n1-n2);
+		printf("n1>n2 %d",n1-n2);
 	}else{
-		printf("n2 is greater than n1 %d",n2-n1);
+		printf("n2>n1 %d",n2-n1);
 	}
 }
