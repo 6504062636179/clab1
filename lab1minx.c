33093aedb00d412dde604c2f2f4d2c0e19b228c8
6504062636179 <153050109+6504062636179@users.noreply.github.com>
2024-11-28 18:07:18 +0700

    use if if

--- lab1minx.c
+++ lab1minx.c
@@ -3,11 +3,11 @@
 
 int main(){
 	
-	int n1 = 10;
-	int n2 = 8;
+	int n1 = 9;
+	int n2 = 10;
 	if(n1 > n2){
 		printf("n1 is greater than n2");
-	}else{
+	}if(n2 > n1){
 		printf("n2 is greater than");
 	}
 }
